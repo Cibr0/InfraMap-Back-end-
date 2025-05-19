539a00dc71f5b6a839e068c54669079aee61bd23
Cibr0 <chavehsuh@gmail.com>
2025-05-19 17:19:33 -0300

    feat: CRUD e login para usuários com bcrypt e tratamento de erros

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt";
 
 const router = express.Router();
+
 // Rota para criar um novo usuário
 router.post("/", async (req, res) => {
   try {
@@ -27,6 +28,86 @@ router.post("/", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Erro ao criar usuário" });
+  }
+});
+
+//login
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Usuário não encontrado" });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(400).json({ message: "Senha incorreta" });
+    }
+
+    res.status(200).json({ message: "Login bem-sucedido", userId: user._id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Erro ao fazer login" });
+  }
+});
+
+// Buscar usuário por ID
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "Usuário não encontrado" });
+    }
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Erro ao buscar usuário" });
+  }
+});
+
+//atualizar usuário
+router.put("/:id", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const updateData = { name, email };
+    if (password) {
+      updateData.password = await bcrypt.hash(password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      updateData,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "Usuário não encontrado" });
+    }
+
+    res.json({ message: "Usuário atualizado com sucesso", updatedUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Erro ao atualizar usuário" });
+  }
+});
+
+//deleta usuaario
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser) {
+      return res.status(404).json({ message: "Usuário não encontrado" });
+    }
+
+    res.json({ message: "Usuário deletado com sucesso" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Erro ao deletar usuário" });
   }
 });
 
